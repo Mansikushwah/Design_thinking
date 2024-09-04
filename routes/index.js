57a2177b987f0cd1a3cd1147b13fee7605b1c73b
Mansi <mansikushwah12345@gmail.com>
2024-09-04 14:29:25 +0530

    added routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('index', { user: req.user });
 });
+router.get('/search_people', (req, res) => {
+    res.render('search_people');
+});
+router.get('/search', (req, res) => {
+    res.render('search');
+});
 
 module.exports = router;
